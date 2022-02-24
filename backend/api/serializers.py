375df4cec3594042cac47e8bd3568f48bf6103f7
Faruq <faruqabdulsalam@yahoo.com>
2022-02-24 12:17:15 +0100

    Serialize all model fields

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import Receipt
 
-class ReciptSerializer(serializers.ModelSerializer):
+class ReceiptSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receipt
-        fields = ('id', 'name', 'address', 'phone', 'total_amount')
+        fields = '__all__'
 
