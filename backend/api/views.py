e28ebb3d5230e58aa18b2294f8077e3cb110fee0
Faruq <faruqabdulsalam@yahoo.com>
2022-02-24 12:17:44 +0100

    Add 'GET' method to receipt view

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,15 +5,21 @@ from rest_framework import status
 
 #import model and model serializer
 from .models import Receipt
-from .serializers import ReciptSerializer
-
+from .serializers import ReceiptSerializer
 
 # Create your views here.
-@api_view(['POST'])
+@api_view(['GET', 'POST'])
 def receipt(request):
-		print(type(request.data))
-        # serializer = NoteSerializer(data=request.data)
-        # if serializer.is_valid():
+	if request.method == 'GET':
+		receipt = Receipt.objects.all()
+		serializer = ReceiptSerializer(receipt, many=True)
+		return Response(serializer.data)
+
+	elif request.method == 'POST':
+		print(request.data)
+		serializer = ReceiptSerializer(data=request.data)
+		if serializer.is_valid():
+			print(serializer)
         #     serializer.save()
         #     return Response(status=status.HTTP_201_CREATED)
         # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
