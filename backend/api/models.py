8a18239f0e4bbcae855692357eb05bfcc953d1c9
Faruq <faruqabdulsalam@yahoo.com>
2022-02-24 11:59:29 +0100

    Create receipt model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+class Receipt(models.Model):
+	name = models.CharField(max_length=120)
+	address = models.CharField(max_length=240)
+	phone = models.CharField(max_length=20)
+	total_amount = models.PositiveIntegerField()
+	created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) 
+
+	def get_absolute_url(self):
+		return reverse('reports:detail', kwargs={'pk': self.pk})
+
+	def __str__(self):
+		return str(self.name)
+
+	class Meta:
+		ordering = ('-created',)
