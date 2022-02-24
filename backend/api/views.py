8de47ec0787d6537ba50326ca2e07b999df0753c
Faruq <faruqabdulsalam@yahoo.com>
2022-02-24 12:09:28 +0100

    import model and model serializer

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+#import model and model serializer
+from .models import Receipt
+from .serializers import ReciptSerializer
+
 
 # Create your views here.
+@api_view(['POST'])
+def receipt(request):
+		print(type(request.data))
+        # serializer = NoteSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
