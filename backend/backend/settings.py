8a65e0bc6db19f692ff5910fdd9bff12f034cc21
Faruq <faruqabdulsalam@yahoo.com>
2022-02-24 10:41:07 +0100

    Protect secret key with python dotenv package

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f5(i)h$*0t@933($mw2fictbue#6*hc&^=#zsg)8(o@l$f4s(@'
+SECRET_KEY = str(os.getenv('SECRET_KEY') or 'you-will-never-know')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
