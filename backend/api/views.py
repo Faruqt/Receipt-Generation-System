c78fae5db5c85e9e866b935cc412188a295a7726
Faruq <faruqabdulsalam@yahoo.com>
2022-02-24 14:50:48 +0100

    Import xhtml2pdf settings

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,6 +7,12 @@ from rest_framework import status
 from .models import Receipt
 from .serializers import ReceiptSerializer
 
+#import settings for converting to pdf
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
 # Create your views here.
 @api_view(['GET', 'POST'])
 def receipt(request):
@@ -19,7 +25,27 @@ def receipt(request):
 		print(request.data)
 		serializer = ReceiptSerializer(data=request.data)
 		if serializer.is_valid():
-			print(serializer)
 			serializer.save()
 			return Response(status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def render_pdf_view(request, pk):
+    template_path = 'api/receipt-pdf.html'
+
+    obj = get_object_or_404(Receipt, pk=pk)
+    context = {'obj': obj}
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="receipt.pdf"'
+
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
