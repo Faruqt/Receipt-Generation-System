902a9d56199ad19fae27b69600e55f90daa3d294
Faruq <faruqabdulsalam@yahoo.com>
2022-02-24 14:55:38 +0100

    Order receipts by time created

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from django.urls import reverse
 
 # Create your models here.
 class Receipt(models.Model):
@@ -9,9 +8,6 @@ class Receipt(models.Model):
 	phone = models.CharField(max_length=20)
 	total_amount = models.PositiveIntegerField()
 	created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) 
-
-	def get_absolute_url(self):
-		return reverse('reports:detail', kwargs={'pk': self.pk})
 
 	def __str__(self):
 		return str(self.name)
