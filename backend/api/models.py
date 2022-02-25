14aca14710b514ff7e46bcde274f9b1abf73d291
Faruq <faruqabdulsalam@yahoo.com>
2022-02-25 07:15:44 +0100

    Add new models receipt objects

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,8 +7,17 @@ class Receipt(models.Model):
 	address = models.CharField(max_length=240)
 	phone = models.CharField(max_length=20)
 	total_amount = models.PositiveIntegerField()
-	receipt = models.FileField(upload_to='receipts', null=True, blank=True)
 	created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) 
+	receipt_1 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_2 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_3 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_4 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_5 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_6 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_7 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_8 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_9 = models.FileField(upload_to='receipts', null=True, blank=True)
+	receipt_10 = models.FileField(upload_to='receipts', null=True, blank=True)
 
 	def __str__(self):
 		return str(self.name)
