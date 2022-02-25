e71ff829e2e9bf95deb23c0070f74eb841e6b624
Faruq <faruqabdulsalam@yahoo.com>
2022-02-25 20:49:32 +0100

    Change API view to GenericAPIView

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-# from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
 from django.contrib.auth.models import User
 from rest_framework.response import Response
