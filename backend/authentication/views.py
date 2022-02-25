5170303cfa86c662e2ef9d379e9d6cecefc9372c
Faruq <faruqabdulsalam@yahoo.com>
2022-02-25 11:21:26 +0100

    Create view to register new users

---
+++ backend/authentication/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status, permissions
+
+#import model and model serializer
+from .serializers import RegisterUserSerializer
+
+class CreateUserView(APIView):
+	#allow new users to sign up and access receipt endpoint
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format='json'):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
