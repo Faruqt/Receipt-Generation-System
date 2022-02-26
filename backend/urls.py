d22d57e0ae2d2a25efa2c89b9dc6ec717c874909
Faruq <faruqabdulsalam@yahoo.com>
2022-02-26 09:42:06 +0100

    Set namespace in base urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -47,8 +47,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pdf_receipt.urls')),
-    path('', include('authentication.urls')),
+    path('', include('pdf_receipt.urls', namespace="pdf")),
+    path('', include('authentication.urls', namespace="auth")),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
